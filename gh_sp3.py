4a794548a5b90a40f890be152362e8ea8326702a
rh privat <shop5@grasmax.de>
2025-07-01 10:45:48 +0200

    Fehlerbehebungen

--- gh_sp3.py
+++ gh_sp3.py
@@ -2,7 +2,6 @@
 # Aufruf, nur Abfrage: python sp3.py [0||1||2]
 # Aufruf, Kanal schalten: python sp3.py [0||1||2] [on||off]
 
-
 import time, sys
 import requests 
 
@@ -15,11 +14,24 @@ def HoleShellyStatus(Id):
       sReq = f'http://{ShellyPro3Ip}/rpc/Switch.GetStatus?id={Id}'
 
       jStat = requests.get( sReq, headers={'Content-Type': 'application/json'}).json()
+      # {'id': 1, 'source': 'HTTP_in', 'output': False, 'temperature': {'tC': 36.2, 'tF': 97.1}}
 
-      print(f'Kanal-Id: {Id}: output: {jStat["output"]}, temperature.tC: {jStat["temperature"]["tC"]}°C')
+      sId = jStat['id'] if (jStat.get('id') != None) else ''
+      sOutput = jStat['output'] if (jStat.get('output') != None) else ''
+      sTemp = f'{jStat["temperature"]["tC"]}°C' if jStat["temperature"]["tC"] != None else ''
+      
+      print(f'Kanal-Id: {sId}: output: {sOutput}, temperature.tC: {sTemp}')
+      
+      if sOutput == False:
+         return 'false'
+      elif sOutput == True:
+         return 'true'
+      else:
+         return ''
 
    except Exception as e:
-      sErr = f'Fehler in HoleShellyStatus(Id={Id}): {e}'
+      print( f'Ausnahme in HoleShellyStatus(Id={Id}): {e}')
+      return -1
 
 ###### ShellySendeSchaltimpuls( Id=[0||1||2], sEinAus=[true||false]) ##############################################################################
 # Id 0 - Out 1/rechts (von vorn gesehen)
@@ -28,9 +40,14 @@ def HoleShellyStatus(Id):
 # Beispiele: 
 # http://192.168.1.100/rpc/Switch.Set?id=1&on=true 
 # http://192.168.1.100/rpc/Switch.Set?id=1&on=false 
+
 def ShellySendeSchaltimpuls( Id, TrueFalse):
 
    try:
+      sOutput = HoleShellyStatus(Id)
+      if TrueFalse == sOutput:
+         print(f'Schalten nicht nötig: {TrueFalse} == {sOutput}') 
+         return
         
       sReq = f"http://{ShellyPro3Ip}/rpc/Switch.Set?id={Id}&on={TrueFalse}"
 
@@ -40,10 +57,16 @@ def ShellySendeSchaltimpuls( Id, TrueFalse):
       
       time.sleep(0.5) # warten, bis das Relais umgeschaltet hat
             
-      HoleShellyStatus( Id)
+      sStat = HoleShellyStatus( Id)
+      if TrueFalse == sStat:
+         print(f'Gewünschter Schaltimpuls {TrueFalse} ausgeführt')
+      else:
+         print(f'Schaltimpuls {TrueFalse} konnte nicht ausgeführt werden. Ergebnis: {sStat}')
+            
 
    except Exception as e:
       print(f'Ausnahme in ShellySendeSchaltimpuls({Id}):  {e}')
+
 
 def main(argv):
 
@@ -61,10 +84,9 @@ def main(argv):
          onoff = argv[2].lower()
 
          if onoff == 'on':
-            ShellySendeSchaltimpuls( Id=id, TrueFalse='true')
+               ShellySendeSchaltimpuls( Id=id, TrueFalse='true')
          elif onoff == 'off':
-            ShellySendeSchaltimpuls( Id=id, TrueFalse='false')
-
+               ShellySendeSchaltimpuls( Id=id, TrueFalse='false')
 
    except Exception as e:
            sErr = f'Ausnahme : {e}'
